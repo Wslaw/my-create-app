c3b49385f1ff542ddbc712a288534af50bb84010
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-06-20 22:08:17 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -4,56 +4,65 @@ import React, { useState } from "react";
 function App() {
   const [primary, setPrimary] = useState("");
   const [unic, setUnic] = useState(false);
-  // if (primary) {
-  //   console.log("primary[1]=>", primary[1]);
-  // }
-  // console.log("Don`t primary");
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleChange = (event) => {
     setPrimary(event.target.value);
     setUnic(false);
+    setIsSubmitted(false);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const arr = primary.split("");
+    // Var-1
     let isUnique = true;
 
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[i] === arr[j]) {
+    // for (let i = 0; i < primary.length; i++) {
+    //   for (let j = i + 1; j < primary.length; j++) {
+    //     if (primary[i] === primary[j]) {
+    //       isUnique = false;
+    //       break;
+    //     }
+    //   }
+    //   if (!isUnique) {
+    //     break;
+    //   }
+    // }
+    // if (isUnique) {
+    //   setUnic(isUnique);
+    // }
+    // setIsSubmitted(true);
+
+    // var-2
+      for (let i = 0; i < primary.length; i++) {
+        const element = primary[i];
+
+        if (primary.lastIndexOf(element) !== i) {
           isUnique = false;
           break;
         }
       }
-      if (!isUnique) {
-        break;
-      }
-    }
-    if (isUnique) {
-      setUnic(true);
-      console.log("Все элементы уникальны");
-    } else {
-      setUnic(false);
-      console.log("Есть повторяющиеся элементы");
-    }
+      if (isUnique) {
+        setUnic(isUnique);
+      } 
+      setIsSubmitted(true);
   };
+
+    
+    // var 3
 
   return (
     <div className={styles.app}>
-      <header className="header">
-        <h1 className={styles.title}> {unic ? "Все элементы уникальны" : "Есть повторяющиеся элементы"}</h1>
-        <p className={styles.txt}> перевірка рядка на унікальність</p>{" "}
+      <header className={styles.header}>
+        <h1 className={styles.title}> перевірка рядка на унікальність</h1>{" "}
         <form onSubmit={handleSubmit} className={styles.form}>
-          <label className={styles.lable}>Ввод рядка</label>
+          <label className={styles.lable}>Введення рядка</label>
           <input value={primary} onChange={handleChange} onSubmit={handleSubmit} className={styles.input} placeholder="Введите рядок символов"></input>
           <button type="submit" className={styles.btn}>
             Submit
           </button>
         </form>
-        <p className={styles.txt}>Current input: {primary}</p>
-        <p className={styles.txt}>Unic: {unic ? "Все элементы уникальны" : "Есть повторяющиеся элементы"}</p>
+        {isSubmitted && <h2 className={styles.txt}> {unic ? "Усі елементи унікальні" : "Є елементи, що повторюються"}</h2>}
       </header>
     </div>
   );
